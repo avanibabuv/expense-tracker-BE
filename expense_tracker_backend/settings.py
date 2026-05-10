68e208132ecc2bf421304e3c0c80b526383d4640
Avani V <avanibabuv@gmail.com>
2026-05-10 19:51:11 +0530

    allowed host updated

--- expense_tracker_backend/settings.py
+++ expense_tracker_backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-a-j%5=5#vn2$5psd-=yzup5v@f4!0@5^3i0mbv1(fbxi1u=i22
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["avaniv.pythonanywhere.com"]
 
 # Application definition
 
