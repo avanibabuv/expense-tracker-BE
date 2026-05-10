417ff2bdbfb7aa48d27278c365b484bc2403c994
Avani V <avanibabuv@gmail.com>
2026-05-10 18:23:58 +0530

    Ai Overview API

--- expenses/views.py
+++ expenses/views.py
@@ -7,6 +7,11 @@ from rest_framework.permissions import IsAuthenticated
 
 from .serializer import ExpenseCreateSerializer,ExpenseListSerializer,ExpenseDeleteSerializer
 from .models import Expense
+from dotenv import load_dotenv
+import os
+from google import genai
+
+load_dotenv()
 
 
 class ExpenseCreateAPI(APIView):
@@ -25,7 +30,9 @@ class ExpenseCreateAPI(APIView):
         expense.full_clean()
         expense.save()
         return Response("Expense Saved Successfully", status=HTTP_201_CREATED)
-    
+
+
+
 
 class ExpenseListAPI(APIView):
 
@@ -47,3 +54,26 @@ class ExpenseDeleteAPI(APIView):
         expense = Expense.objects.get(id=serializer.validated_data["id"])
         expense.delete()
         return Response("Expense Deleted Successfully", status=HTTP_201_CREATED)
+    
+
+
+class ExpenseAiOverviewAPI(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        expenses = Expense.objects.filter(user=user)
+        data = list(expenses.values("category", "amount", "created_date", "note"))
+        prompt = f"""
+            Analyse this expense data and give a short summary and advice
+
+            {data}
+
+            keep it simple and user friendly.
+        """
+        client = genai.Client()
+        response = client.models.generate_content(
+            model="gemini-3-flash-preview", contents=prompt
+        )
+        return Response(response.text, status=HTTP_200_OK)
